98b577b197ff3fdb1b75bc173b18c2a6a862646c
JavDaGreat <98164907+JavDaGreat@users.noreply.github.com>
2023-08-07 17:29:07 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import SlideShow from '../components/SlideShow'
 import Header from '../components/Header'
-import Cart from '../components/cart'
+import Cart from '../components/Cart'
 
 
 function Home() {
