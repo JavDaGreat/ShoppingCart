b976a5c9795ab326d9789c453b775a71170837be
Javad <1998javad@gmail.com>
2024-11-06 13:48:27 +0100

    Fix image paths in product_API.json and improve formatting in ProductShow component

--- src/components/ProductShow.jsx
+++ src/components/ProductShow.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { addProduct } from '../store/productsSlice'
+import { useDispatch } from "react-redux";
+import { addProduct } from "../store/productsSlice";
 
-
-function ProductShow({title,price,image,id}) {
-  const dispatch=useDispatch()
-  function handleAdd(){
-    dispatch(addProduct({title,price,id,qty:1,image:image[0]}))
+function ProductShow({ title, price, image, id }) {
+  const dispatch = useDispatch();
+  function handleAdd() {
+    dispatch(addProduct({ title, price, id, qty: 1, image: image[0] }));
   }
   return (
-    <div className='flex flex-col justify-center items-center py-8 px-4 shadow-2xl shadow-gray-200 '>
-      <img src={image[0]} alt=""  className='object-contain w-96 h-48' />
-      <h1 className='font-bold py-4'>{title}</h1>
-      <h2 className='py-2'>{price}$</h2>
-      <button onClick={handleAdd} className='bg-gray-300 md:w-[30%] rounded-lg px-2 py-2 hover:-translate-y-[2px]  focus: shadow-lg transform active:scale-90 transition-transform '>Add to cart</button>
+    <div className="flex flex-col justify-center items-center py-8 px-4 shadow-2xl shadow-gray-200 ">
+      <img src={image[0]} alt="" className="object-contain w-96 h-48" />
+      <h1 className="font-bold py-4">{title}</h1>
+      <h2 className="py-2">{price}$</h2>
+      <button
+        onClick={handleAdd}
+        className="bg-gray-300 md:w-[30%] rounded-lg px-2 py-2 hover:-translate-y-[2px]  focus: shadow-lg transform active:scale-90 transition-transform ">
+        Add to cart
+      </button>
     </div>
-  )
+  );
 }
 
-export default ProductShow
+export default ProductShow;
